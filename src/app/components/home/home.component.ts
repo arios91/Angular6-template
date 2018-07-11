187469823e423f0ce47e897826664e9565790250
Alejandro Rios <Alx.Rios91@gmail.com>
2018-07-11 00:12:01 -0500

    finished galleria along with redirect links, fixed logo and responsive breaks

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { SeoService } from '../../services/seo.service';
 import { ContactService } from '../../services/contact.service';
 import { GalleriaModule } from 'primeng/galleria';
@@ -11,6 +11,9 @@ import { GalleriaModule } from 'primeng/galleria';
 export class HomeComponent implements OnInit {
 
   images: any[];
+  modalDisplay:string = 'none';
+  redirectPageName: string = '';
+  redirectPageURL: string = '';
 
   constructor(
     private seoService: SeoService,
@@ -21,13 +24,40 @@ export class HomeComponent implements OnInit {
     this.seoService.generateTags({
     })
     this.images = [];
-    this.images.push({source:'assets/images/petalosArteScreenshot.png', alt:'Petalos y Arte Flower Shop Screenshot', title:'Petalos y Arte Flower Shop'});
-    // this.images.push({source:'assets/images/galleria4.jpg', alt:'Description for Image 2', title:'Title 2'});
-    // this.images.push({source:'assets/images/galleria5.jpg', alt:'Description for Image 3', title:'Title 3'});
+    this.images.push({source:'assets/images/petalosArteScreenshot.png',
+      alt:'Locally owned flower shop in Palmview, TX which provides local fresh flower delivery.'
+      + ' Their conversion centered eCommerce site has drastically increased revenue and helped locals discover the location. '
+      + 'click on the image above to visit Petalos y Arte Flower Shops site.',
+
+
+      title:'Petalos y Arte Flower Shop'});
+    // this.images.push({source:'assets/images/galleria4.jpg',
+    //   alt:'Description for Image 2',
+    //   title:'Title 2'});
+    // this.images.push({source:'assets/images/galleria5.jpg',
+    //   alt:'Are you ready to have your business found on the internet! Click the contact button and get in touch so we can get started!',
+    //   title:'Your website could be here!'});
   }
 
   testClick(){
     this.contact.sendContactEmail('someGuy@gmail.com', 'plain text version', '<b>bold html version</b>');
+  }
+
+  testFunction(index:any){
+    if(index == 0){
+      this.modalDisplay = 'block';
+      this.redirectPageName = 'Petalos y Arte Flower Shop';
+      this.redirectPageURL = 'https://www.petalosarte.com';
+    }
+  }
+  
+  redirect(){
+    this.modalDisplay = 'none';
+    window.open(this.redirectPageURL, '_blank');
+  }
+
+  closeModal(){
+    this.modalDisplay = 'none';
   }
 
 }
